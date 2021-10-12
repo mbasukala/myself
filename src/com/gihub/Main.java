a8ca8e4e4e9589ccb3bc6f2eb87f6b7bc7650f08
mbasukala <mbasukala002@gmail.com>
2021-10-12 00:19:04 -0400

    changes

--- src/com/gihub/Main.java
+++ src/com/gihub/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         House buildiing = new House(4.2F, 3, 4, 2, "white" );
         System.out.println(buildiing.getArea());
+        System.out.println(buildiing.getArea());
     }
 }
